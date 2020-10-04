b3a471a62894c867809a015ddaaf11dea8a9e30a
friedPotat0 <5374007+friedPotat0@users.noreply.github.com>
2020-10-05 01:45:35 +0200

    remove upper and lower bounds formatting in options

--- options.js
+++ options.js
@@ -4,15 +4,11 @@ const DEFAULT_SCORE_UPPER_BOUNDS = 2.0
 async function init() {
   let storage = await browser.storage.local.get(['scoreIconLowerBounds', 'scoreIconUpperBounds'])
   let lowerBounds = parseFloat(
-    storage && storage.scoreIconLowerBounds !== undefined
-      ? storage.scoreIconLowerBounds
-      : DEFAULT_SCORE_LOWER_BOUNDS.toFixed(2)
-  ).toFixed(2)
+    storage && storage.scoreIconLowerBounds !== undefined ? storage.scoreIconLowerBounds : DEFAULT_SCORE_LOWER_BOUNDS
+  )
   let upperBounds = parseFloat(
-    storage && storage.scoreIconLowerBounds !== undefined
-      ? storage.scoreIconUpperBounds
-      : DEFAULT_SCORE_UPPER_BOUNDS.toFixed(2)
-  ).toFixed(2)
+    storage && storage.scoreIconLowerBounds !== undefined ? storage.scoreIconUpperBounds : DEFAULT_SCORE_UPPER_BOUNDS
+  )
   document.querySelector('#score-bounds-lower').value = lowerBounds
   document.querySelector('#score-bounds-upper').value = upperBounds
   document.querySelector('.score-bounds-lower-value').textContent = lowerBounds
@@ -31,22 +27,22 @@ async function save() {
   let newLowerBounds, newUpperBounds
   let storage = await browser.storage.local.get(['scoreIconLowerBounds', 'scoreIconUpperBounds'])
   try {
-    newLowerBounds = parseFloat(document.querySelector('#score-bounds-lower').value).toFixed(2)
-    newUpperBounds = parseFloat(document.querySelector('#score-bounds-upper').value).toFixed(2)
+    newLowerBounds = parseFloat(document.querySelector('#score-bounds-lower').value)
+    newUpperBounds = parseFloat(document.querySelector('#score-bounds-upper').value)
     if (!(newLowerBounds <= 0 && newUpperBounds >= -10000)) {
       newLowerBounds = parseFloat(
         storage && storage.scoreIconLowerBounds !== undefined
           ? storage.scoreIconLowerBounds
-          : DEFAULT_SCORE_LOWER_BOUNDS.toFixed(2)
-      ).toFixed(2)
+          : DEFAULT_SCORE_LOWER_BOUNDS
+      )
       throw Error('Wrong score lower bounds')
     }
     if (!(newUpperBounds >= 0 && newUpperBounds <= 10000)) {
       newUpperBounds = parseFloat(
         storage && storage.scoreIconLowerBounds !== undefined
           ? storage.scoreIconUpperBounds
-          : DEFAULT_SCORE_UPPER_BOUNDS.toFixed(2)
-      ).toFixed(2)
+          : DEFAULT_SCORE_UPPER_BOUNDS
+      )
       throw Error('Wrong score upper bounds')
     }
     browser.storage.local.set({
@@ -57,15 +53,15 @@ async function save() {
     console.error(err)
   }
   document.querySelector('#score-bounds-lower').value =
-    newLowerBounds !== null ? newLowerBounds : DEFAULT_SCORE_LOWER_BOUNDS.toFixed(2)
+    newLowerBounds !== null ? newLowerBounds : DEFAULT_SCORE_LOWER_BOUNDS
   document.querySelector('#score-bounds-upper').value =
-    newUpperBounds !== null ? newUpperBounds : DEFAULT_SCORE_UPPER_BOUNDS.toFixed(2)
+    newUpperBounds !== null ? newUpperBounds : DEFAULT_SCORE_UPPER_BOUNDS
   document.querySelector('.score-bounds-lower-value').textContent =
-    newLowerBounds !== null ? newLowerBounds : DEFAULT_SCORE_LOWER_BOUNDS.toFixed(2)
+    newLowerBounds !== null ? newLowerBounds : DEFAULT_SCORE_LOWER_BOUNDS
   document.querySelector('.score-bounds-upper-value').textContent =
-    newUpperBounds !== null ? newUpperBounds : DEFAULT_SCORE_UPPER_BOUNDS.toFixed(2)
+    newUpperBounds !== null ? newUpperBounds : DEFAULT_SCORE_UPPER_BOUNDS
   ;(await messenger.runtime.getBackgroundPage()).messenger.SpamScores.setScoreBounds(
-    parseFloat(newLowerBounds !== null ? newLowerBounds : DEFAULT_SCORE_LOWER_BOUNDS).toFixed(2),
-    parseFloat(newUpperBounds !== null ? newUpperBounds : DEFAULT_SCORE_UPPER_BOUNDS).toFixed(2)
+    parseFloat(newLowerBounds !== null ? newLowerBounds : DEFAULT_SCORE_LOWER_BOUNDS),
+    parseFloat(newUpperBounds !== null ? newUpperBounds : DEFAULT_SCORE_UPPER_BOUNDS)
   )
 }
